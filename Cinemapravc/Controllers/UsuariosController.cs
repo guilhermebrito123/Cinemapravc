83f98e58dd87bafdd414cdde516bc6d42cb0c7ec
guilhermebrito123 <gbfsilva@sga.pucminas.br>
2024-08-27 12:01:51 -0300

    Criando o controller Usuarios, junto com seu método ActionResult Index, junto com sua View Index

---
+++ Cinemapravc/Controllers/UsuariosController.cs
@@ -0,0 +1,18 @@
+﻿using Cinemapravc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cinemapravc.Controllers
+{
+    public class UsuariosController : Controller 
+    {
+        private readonly AppDbContext _context;
+        public UsuariosController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}   
